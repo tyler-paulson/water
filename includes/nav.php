ff2e630d866b986a8361a32d2932034e428428f4
Tyler Paulson <me@tylerpaulson.com>
2017-04-08 10:52:00 -0400

    Closes #2 - Add Privacy Policy

--- includes/nav.php
+++ includes/nav.php
@@ -3,5 +3,8 @@
 	<div class="navbar-header">
 	  <a class="navbar-brand" href="/">Fitbit Water Bttn</a>
 	</div>
+	<ul class="nav navbar-nav navbar-right">
+	  <li><a href="/privacy.php">Privacy</a></li>
+	</ul>
   </div>
 </nav>
---
+++ privacy.php
@@ -0,0 +1,31 @@
+<?php require_once('config.php'); ?>
+
+<html>
+
+<head>
+
+	<title>Fitbit Water Bttn | Privacy</title>
+
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+</head>
+
+<body>
+
+<?php include('includes/nav.php'); ?>
+
+<div class="container">
+	<div class="page-header">
+	  <h1>Privacy Policy</h1>
+	</div>
+	<p>FitBit Water Button stores an access token and a refresh token in our database alongside your FitBit user ID.</p>
+	<p>None of your personal health data (steps, water consumed, etc.) is requested from FitBit nor stored in our database.</p>
+</div>
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
