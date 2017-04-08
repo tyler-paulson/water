da30031e5834e0ebe192d981fa58f5880d3f5975
Tyler Paulson <me@tylerpaulson.com>
2017-04-08 10:58:26 -0400

    Update instructions

--- config-sample.php
+++ config-sample.php
@@ -9,6 +9,6 @@ define('MYSQL_HOST', 'localhost');
 define('MYSQL_USER', 'mysql_user_here');
 define('MYSQL_PASS', 'mysql_pass_here');
 define('MYSQL_DB', 'mysql_db_here'); 
-// user needs input and select privledges on the database
+// user needs input, update and select privledges on the database
 
 ?>
